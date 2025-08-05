a9cf383fd109602a4532987c8e898792497f895a
Manu Manzano <mmanzano1993@gmail.com>
2025-08-05 07:28:27 +0200

    Primer Commit del Portfolio
    Primeros diseños de distribución y apariencia.
    NAV, About ME & PROJECTS

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+AOS.init();
+new Typed("#typed", {
+  strings: ["Manuel Manzano"],
+  typeSpeed: 50,
+  showCursor: true,
+  cursorChar: "|", // Aquí puedes cambiarlo si quieres
+  loop: false
+});
+
+});
+
