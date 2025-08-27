80f618d83aff89721bb86c076aced71702ff5486
Manu Manzano <mmanzano1993@gmail.com>
2025-08-27 18:55:50 +0200

    Cambios en el diseño, funcionamiento y disposición.

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-AOS.init();
-new Typed("#typed", {
-  strings: ["Manuel Manzano"],
-  typeSpeed: 50,
-  showCursor: true,
-  cursorChar: "|", // Aquí puedes cambiarlo si quieres
-  loop: false
-});
+  AOS.init();
+  new Typed("#typed", {
+    strings: ["Manuel Manzano"],
+    typeSpeed: 50,
+    showCursor: true,
+    cursorChar: "|",
+    loop: false,
+  });
+  let navLinks = document.querySelectorAll("#navbarNav .nav-link");
+  let navbarCollapse = document.getElementById("navbarNav");
 
-});
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false,
+      });
+      bsCollapse.hide();
+    });
+  });
 
+  
+});
